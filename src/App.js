74781e8053f9a2b9bd015d6c87327582de0f447d
Diana Silva <diana.m.silva22@gmail.com>
2020-02-10 23:38:54 +0000

    Creates a shop page and a collection preview component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
